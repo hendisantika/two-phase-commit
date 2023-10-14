145b830f6a9caece69509abd2144622349d4cd04
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-14 07:35:57 +0700

    #18 - Add TransactionData in payment service 💸💰:octocat:💵

---
+++ payment/src/main/java/com/hendisantika/payment/dto/TransactionData.java
@@ -0,0 +1,24 @@
+package com.hendisantika.payment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : two-phase-commit
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/14/2023
+ * Time: 7:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class TransactionData {
+    private String orderNumber;
+    private String item;
+    private String price;
+    private String paymentMode;
+}
