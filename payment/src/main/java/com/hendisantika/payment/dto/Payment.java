e04c6a9c1aaa6e7ab8bbb03b6df259982b40b58b
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-14 07:34:46 +0700

    #16 - Add Payment DTO in payment service 💸💰:octocat:💵

---
+++ payment/src/main/java/com/hendisantika/payment/dto/Payment.java
@@ -0,0 +1,38 @@
+package com.hendisantika.payment.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : two-phase-commit
+ * User: hendi
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Link : s.id/hendisantika
+ * Date: 10/14/2023
+ * Time: 7:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    private String item;
+
+    private String preparationStatus;
+
+    private String paymentMode;
+
+    private String price;
+}
