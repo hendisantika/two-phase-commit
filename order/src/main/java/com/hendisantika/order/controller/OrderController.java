fb5f2bec4c1d937dc92211f49baa2b1aa5070b4f
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-14 07:27:48 +0700

    #9 - Add prepareOrder in OrderController in order service 💸📗:octocat:💵

--- order/src/main/java/com/hendisantika/order/controller/OrderController.java
+++ order/src/main/java/com/hendisantika/order/controller/OrderController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.order.controller;
 
+import com.hendisantika.order.dto.TransactionData;
+import com.hendisantika.order.entity.Order;
+import com.hendisantika.order.entity.OrderPreparationStatus;
 import com.hendisantika.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,4 +29,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderRepository orderRepository;
+
+    @PostMapping("/prepare_order")
+    public ResponseEntity<String> prepareOrder(@RequestBody TransactionData transactionData) {
+        try {
+            Order order = new Order();
+            order.setOrderNumber(transactionData.getOrderNumber());
+            order.setItem(transactionData.getItem());
+            order.setPreparationStatus(OrderPreparationStatus.PREPARING.name());
+            orderRepository.save(order);
+
+            if (shouldFailedDuringPrepare()) {
+                throw new RuntimeException("Prepare phase failed for order " + transactionData.getOrderNumber());
+            }
+
+            return ResponseEntity.ok("Order prepared successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error during oredr preparation");
+        }
+    }
 }
